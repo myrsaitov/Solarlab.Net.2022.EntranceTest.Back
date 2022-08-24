79ffc8ee443ac486a444e418018ea5e7680b3761
mirkm <mirkm@tavrida.com>
2022-08-24 16:53:16 +0300

    Исправлено сидирование категорий

--- src/Congratulations/Infrastructure/Congratulations.DataAccess/DataAccess/EntitiesConfiguration/CategoryConfiguration.cs
+++ src/Congratulations/Infrastructure/Congratulations.DataAccess/DataAccess/EntitiesConfiguration/CategoryConfiguration.cs
@@ -7,11 +7,11 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
 {
     public class CategoryConfiguration : IEntityTypeConfiguration<Category>
     {
-        // Группа 1 - "Транспорт"
+        // Группа 1 - "Личные праздники"
         private static Category category1 = new Category
         {
             Id = 1,
-            Name = "Транспорт",
+            Name = "Личные праздники",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategory = null
@@ -20,35 +20,24 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
         private static Category category1_1 = new Category
         {
             Id = 2,
-            Name = "Автомобили",
+            Name = "Дни рождения",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 1
         };
-
         private static Category category1_2 = new Category
         {
             Id = 3,
-            Name = "Мотоциклы и мототехника",
+            Name = "Дни свадьбы",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 1
         };
-
-        private static Category category1_3 = new Category
-        {
-            Id = 4,
-            Name = "Грузовики и спецтехника",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 1
-        };
-
-        // Группа 2 - "Недвижимость"
+        // Группа 2 - "Гражданские праздники"
         private static Category category2 = new Category
         {
             Id = 5,
-            Name = "Недвижимость",
+            Name = "Гражданские праздники",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategory = null
@@ -57,7 +46,7 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
         private static Category category2_1 = new Category
         {
             Id = 6,
-            Name = "Квартиры",
+            Name = "День шахматиста",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 5
@@ -66,7 +55,7 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
         private static Category category2_2 = new Category
         {
             Id = 7,
-            Name = "Комнаты",
+            Name = "День пожарника",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 5
@@ -75,17 +64,17 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
         private static Category category2_3 = new Category
         {
             Id = 8,
-            Name = "Дома, дачи, коттеджи",
+            Name = "День рыбака",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 5
         };
 
-        // Группа 3 - "Электроника"
+        // Группа 3 - "Политические праздники"
         private static Category category3 = new Category
         {
             Id = 9,
-            Name = "Электроника",
+            Name = "Политические праздники",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategory = null
@@ -94,7 +83,7 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
         private static Category category3_1 = new Category
         {
             Id = 10,
-            Name = "Аудио и видео",
+            Name = "День России",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 9
@@ -102,7 +91,7 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
         private static Category category3_2 = new Category
         {
             Id = 11,
-            Name = "Игры, приставки и программы",
+            Name = "День Конституции",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 9
@@ -110,78 +99,10 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
         private static Category category3_3 = new Category
         {
             Id = 12,
-            Name = "Настольные компьютеры",
+            Name = "День флота",
             CreatedAt = DateTime.UtcNow,
             IsDeleted = false,
             ParentCategoryId = 9
-        };
-        private static Category category3_4 = new Category
-        {
-            Id = 13,
-            Name = "Ноутбуки",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 9
-        };
-        private static Category category3_5 = new Category
-        {
-            Id = 14,
-            Name = "Планшеты и электронные книги",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 9
-        };
-        private static Category category3_6 = new Category
-        {
-            Id = 15,
-            Name = "Телефоны",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 9
-        };
-        private static Category category3_7 = new Category
-        {
-            Id = 16,
-            Name = "Товары для компьютера",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 9
-        };
-        private static Category category3_8 = new Category
-        {
-            Id = 17,
-            Name = "Фототехника",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 9
-        };
-
-        // Группа 4 - "Искусство"
-        private static Category category4 = new Category
-        {
-            Id = 18,
-            Name = "Искусство",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategory = null
-        };
-
-        private static Category category4_1 = new Category
-        {
-            Id = 19,
-            Name = "Живопись маслом",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 18
-        };
-
-        private static Category category4_2 = new Category
-        {
-            Id = 20,
-            Name = "Акварель",
-            CreatedAt = DateTime.UtcNow,
-            IsDeleted = false,
-            ParentCategoryId = 18
         };
 
 
@@ -190,7 +111,6 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
             category1,
             category1_1,
             category1_2,
-            category1_3,
             category2,
             category2_1,
             category2_2,
@@ -199,14 +119,6 @@ namespace Sev1.Congratulations.DataAccess.EntitiesConfiguration
             category3_1,
             category3_2,
             category3_3,
-            category3_4,
-            category3_5,
-            category3_6,
-            category3_7,
-            category3_8,
-            category4,
-            category4_1,
-            category4_2,
         };
         
         public void Configure(EntityTypeBuilder<Category> builder)
